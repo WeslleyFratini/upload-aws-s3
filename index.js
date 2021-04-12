0897c098e58cfe26c7e3c86b33a190ffc1ad1972
WeslleyFratini <weeslleey354@gmail.com>
2021-04-12 19:35:35 -0300

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv/config')
+const multer = require('multer')
+const express = require('express')
+const AWS = require('aws-sdk')
+
+const app = express()
+const port = 3000
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET
+})
+
+const storage = multer.memoryStorage({
+    destination: function(req, file, callback){
+        callback(null, '')
+    }
+})
+
+const upload = multer({storage}).single('image')
+
+
+app.post('/upload', upload, (req,res) => {
+
+    console.log(req.file);
+    res.send({
+        message:"Hello"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is up at ${port}`);
+})
