676a7941dc4317cb91f21c1fe7369b01525abb94
WeslleyFratini <weeslleey354@gmail.com>
2021-04-13 09:19:32 -0300

    upload image

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv/config')
-const multer = require('multer')
+
 const express = require('express')
+const multer = require('multer')
 const AWS = require('aws-sdk')
 const uuid = require('uuid')
 
@@ -13,22 +14,33 @@ const s3 = new AWS.S3({
 })
 
 const storage = multer.memoryStorage({
-    destination: function(req, file, callback){
+    destination: function(req, file, callback) {
         callback(null, '')
     }
 })
 
 const upload = multer({storage}).single('image')
 
+app.post('/upload',upload,(req, res) => {
 
-app.post('/upload', upload, (req,res) => {
+    let myFile = req.file.originalname.split(".")
+    const fileType = myFile[myFile.length - 1]
 
-    console.log(req.file);
-    res.send({
-        message:"Hello"
+    const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `${uuid()}.${fileType}`,
+        Body: req.file.buffer
+    }
+
+    s3.upload(params, (error, data) => {
+        if(error){
+            res.status(500).send(error)
+        }
+
+        res.status(200).send(data)
     })
 })
 
 app.listen(port, () => {
-    console.log(`Server is up at ${port}`);
+    console.log(`Server is up at ${port}`)
 })
